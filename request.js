0ffae2ee4659c80e4e1ad39dc4c899497099dddb
Artyom <rusartdub@gmail.com>
2018-09-29 18:32:41 +0300

    jopa

--- request.js
+++ request.js
@@ -2,7 +2,7 @@ $.ajax({
     dataType: 'json',
     url: 'https://c6b06b47.ngrok.io/execute?lat=5&lon=3',
     success: function(jsondata){
-        //console.log(jsondata);
+        console.log(jsondata);
     }
 
 });
