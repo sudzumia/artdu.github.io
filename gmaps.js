3eb6f15373729bda9eb8b86416a78ce25fc476ab
Artyom <rusartdub@gmail.com>
2018-09-29 20:17:15 +0300

    jopa

--- gmaps.js
+++ gmaps.js
@@ -122,7 +122,7 @@ function initMap() {
                 marker = new google.maps.Marker({
                     position: newPoint,
                     map: map,
-                    type: 'info'
+                    icon: "https://maps.google.com/mapfiles/kml/shapes/info.icon"
                 });
 
 
