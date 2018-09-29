884fcd18019914179a9286a110e01be63b91d0a7
Artyom <rusartdub@gmail.com>
2018-09-29 19:55:02 +0300

    jopa

--- gmaps.js
+++ gmaps.js
@@ -10,7 +10,7 @@ var customLabel = {
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: new google.maps.LatLng(-33.863276, 151.207977),
-        zoom: 2,
+        zoom: 12,
         disableDefaultUI: true,
         mapTypeId: 'terrain',
         styles: [
